f27a21382d4a758c15881430ae9cececebdc6bd0
ctestagrose <73864022+ctestagrose@users.noreply.github.com>
2021-09-01 16:20:36 -0400

    Add files via upload

---
+++ EchoEchoEcho/cmake-build-debug/CMakeFiles/EchoEchoEcho.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/EchoEchoEcho.dir/main.cpp.obj"
+  "EchoEchoEcho.exe"
+  "EchoEchoEcho.exe.manifest"
+  "EchoEchoEcho.pdb"
+  "libEchoEchoEcho.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/EchoEchoEcho.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ EchoEchoEcho/main.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    string word;
+    getline(cin, word);
+    cout << word <<" "<<word<<" "<<word;
+
+}
+
