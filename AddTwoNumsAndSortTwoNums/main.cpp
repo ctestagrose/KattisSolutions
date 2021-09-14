b42b28b36774a9472d8b883c2f432e17760cbd00
Conrad Testagrose <73864022+ctestagrose@users.noreply.github.com>
2021-09-13 23:40:00 -0400

    Add files via upload

---
+++ AddTwoNumsAndSortTwoNums/main.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+int main() {
+
+    int a;
+    int b;
+
+    std::cin >> a;
+    std::cin >> b;
+/*
+    if(a > b){
+
+        printf("%d %d\n", b, a);
+
+    }
+    else{
+
+        printf("%d %d\n", a, b);
+
+    }
+*/
+
+    printf("%d\n", a + b);
+
+
+
+    return 0;
+}
